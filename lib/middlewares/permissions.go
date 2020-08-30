065067870e8b05123163d31e934065c158ed3905
长江 <zhaochangjiang@huoyunren.com>
2020-08-30 18:25:26 +0800

    设置跨域通配符

--- lib/middlewares/permissions.go
+++ lib/middlewares/permissions.go
@@ -111,7 +111,8 @@ func cors(c *gin.Context) (exitStatus bool) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Max-Age", "1800")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE,PATCH")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding,referrer, Authorization, X-*")
+	// c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding,referrer, Authorization, x-*,X-*")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 	c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	return
