2e4996c403c5b933717532b14012483bb0106393
长江 <zhaochangjiang@huoyunren.com>
2020-09-06 00:08:18 +0800

    修改上下文对象初始化

--- lib/base/dao.go
+++ lib/base/dao.go
@@ -12,13 +12,16 @@ type ServiceDao struct {
 }
 
 func (r *ServiceDao) SetContext(context ...*Context) (s *ServiceDao) {
+	for _, cont := range context {
+		cont.InitContext()
+	}
 	switch len(context) {
 	case 0:
 		r.Context = NewContext()
 		break
 	case 1:
 		r.Context = context[0]
-		break
+ 		break
 	default:
 		panic("你传递的参数当前不支持")
 	}
--- lib/base/service.go
+++ lib/base/service.go
@@ -5,13 +5,15 @@ type ServiceBase struct {
 }
 
 func (r *ServiceBase) SetContext(context ...*Context) (s *ServiceBase) {
+	for _, cont := range context {
+		cont.InitContext()
+	}
 	switch len(context) {
 	case 0:
 		r.Context = NewContext()
 		break
 	case 1:
 		r.Context = context[0]
-		r.Context.InitContext()
 		break
 	default:
 		panic("你传递的参数当前不支持")
