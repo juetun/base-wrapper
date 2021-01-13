dc59ab8dca0a9e9a6397c5a9367901ecbad23367
changjiang <zhaochangjiang@modian.com>
2021-01-13 10:47:31 +0800

    添加获取当前机器的IP地址接口

---
+++ example/main..go
@@ -0,0 +1,14 @@
+// @Copyright (c) 2021.
+// @Author ${USER}
+// @Date ${DATE}
+package main
+
+import (
+	"fmt"
+
+	"github.com/juetun/base-wrapper/lib/utils"
+)
+
+func main() {
+	fmt.Println(utils.GetLocalIP())
+}
