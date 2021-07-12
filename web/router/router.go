da8b0d72939f7579cf8aa72d89d655704655141c
changjiang <zhaochangjiang@modian.com>
2021-07-12 10:44:23 +0800

    1

--- web/router/router.go
+++ web/router/router.go
@@ -1,4 +1,4 @@
-// @Copyright (c) 2020. 
+// @Copyright (c) 2020.
 // @Author ${USER}
 // @Date ${DATE}
 package router
@@ -6,8 +6,9 @@ package router
 import (
 	_ "github.com/juetun/base-wrapper/web/router/intranet" // 加载内网访问路由
 	_ "github.com/juetun/base-wrapper/web/router/outernet" // 加载外网访问路由
+	_ "github.com/juetun/base-wrapper/web/router/admin" // 加载超管访问路由
 	_ "github.com/juetun/base-wrapper/web/router/page" // 加载网页访问路由
 )
 func init()  {
-	
+
 }
