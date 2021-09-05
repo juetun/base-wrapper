6b3de22fa527fbf240033180bbe35059e0519078
长江 <zhaochangjiang@huoyunren.com>
2021-09-05 22:35:06 +0800

    1

--- lib/app/middlewares/middleware.go
+++ lib/app/middlewares/middleware.go
@@ -3,12 +3,13 @@ package middlewares
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/common"
 )
 
 var MiddleWareComponent = []gin.HandlerFunc{
-	ErrorHandler(),                  // 捕捉程序异常操作
-	CrossOriginResourceSharing(),    // 配置跨域逻辑
-	GinLogCollect(), // 日志操作逻辑
+
+	CrossOriginResourceSharing(), // 配置跨域逻辑
+	GinLogCollect(),              // 日志操作逻辑
 }
 var io = base.NewSystemOut().SetInfoType(base.LogLevelInfo)
 
@@ -16,6 +17,12 @@ var io = base.NewSystemOut().SetInfoType(base.LogLevelInfo)
 func LoadMiddleWare(privateMiddleWares ...gin.HandlerFunc) {
 
 	io.SystemOutPrintln("Load GIN middleWare start")
+	switch common.GetEnv() {
+	case gin.ReleaseMode: // 线上环境增加异常捕获
+		// 捕捉程序异常操作
+		MiddleWareComponent = append([]gin.HandlerFunc{ErrorHandler()}, MiddleWareComponent...)
+	default:
+	}
 
 	MiddleWareComponent = append(MiddleWareComponent, privateMiddleWares...)
 
