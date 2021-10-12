e6b142229d42e8b2b9aa5b1c00674eae91041ab2
changjiang <zhaochangjiang@modian.com>
2021-10-12 10:03:20 +0800

    添加用户信息

---
+++ lib/common/parameters/user.go
@@ -0,0 +1,30 @@
+package parameters
+
+import (
+	"time"
+)
+
+// 获取用户信息的响应参数结构
+type (
+	ResultUser struct {
+		List map[string]ResultUserItem `json:"list"`
+	}
+	ResultUserItem struct {
+		UserHid          string     `json:"user_hid,omitempty"`  // 用户ID
+		Portrait         string     `json:"portrait,omitempty"`  // 头像
+		NickName         string     `json:"nick_name,omitempty"` // 昵称
+		UserName         string     `json:"user_name,omitempty"` // 用户名
+		Gender           int        `json:"gender,omitempty"`    //
+		Status           int        `json:"status,omitempty"`    //
+		Score            int        `json:"score,omitempty"`     //
+		AuthDesc         string     `json:"auth_desc,omitempty"` // 认证描述
+		IsV              int        `json:"is_v,omitempty"`      // 用户头像加V
+		Signature        string     `json:"signature,omitempty"`
+		RegisterChannel  string     `json:"register_channel,omitempty"`
+		CountryCode      string     `json:"country_code,omitempty"`
+		Mobile           string     `json:"mobile,omitempty"`
+		MobileVerifiedAt *time.Time `json:"mobile_verified_at,omitempty"`
+		Email            string     `json:"email,omitempty"`
+		EmailVerifiedAt  *time.Time `json:"email_verified_at,omitempty"`
+	}
+)
