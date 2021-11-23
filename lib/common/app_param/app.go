0df779f67f8ba16c8d03d7b9b4ebf924f5930e71
changjiang <zhaochangjiang@modian.com>
2021-11-23 11:13:42 +0800

    应用配置

---
+++ lib/common/app_param/app.go
@@ -0,0 +1,18 @@
+package app_param
+
+const (
+
+	AppNameAdmin   = "admin-main"
+	AppNameUpload  = "api-upload"
+	AppNameExport  = "api-export"
+	AppNameUser    = "api-user"
+	AppNameTag     = "api-tag"
+	AppNameComment = "api-comment"
+	AppNameChat    = "api-chat"
+
+	AppNameCart = "api-car"
+
+	AppNameMall         = "api-mall"
+	AppNameMallActivity = "api-activity"
+	AppNameMallOrder    = "api-order"
+)
