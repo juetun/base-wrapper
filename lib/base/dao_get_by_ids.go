c9e2123f36e72acb52d1f4f1f2e5627ce8d9f053
changjiang <zhaochangjiang@modian.com>
2022-03-08 10:51:48 +0800

    12

--- lib/base/dao_get_by_ids.go
+++ lib/base/dao_get_by_ids.go
@@ -1,5 +1,7 @@
 package base
 
+import "fmt"
+
 // 获取数据的常用类型定义
 const (
 	GetDataTypeFromDb    = "db"    // 从数据库获取
@@ -37,14 +39,28 @@ type (
 	//}
 
 	GetDataTypeCommon struct {
-		GetType string `json:"get_type" form:"get_type"`
+		GetType      string `json:"get_type" form:"get_type"`
+		RefreshCache uint8  `json:"refresh_cache" form:"refresh_cache"`
 	}
 )
 
-func (r *GetDataTypeCommon) Default() {
+func (r *GetDataTypeCommon) Default() (err error) {
 	if r.GetType == "" { // 默认是从缓存拿，如果拿不到，则从数据库拿
 		r.GetType = GetDataTypeFromAll
 	}
+	RefreshCacheValue := []uint8{0, 1}
+	var f bool
+	for _, value := range RefreshCacheValue {
+		if value == r.RefreshCache {
+			f = true
+			break
+		}
+	}
+	if !f {
+		err = fmt.Errorf("refresh_cache值异常")
+		return
+	}
+	return
 }
 
 //func(r *GetDataWithString)GetSKuBySkuIds(arg *ArgGetByStringIds) (res map[string]*models.Sku, err error) {
