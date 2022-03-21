8a838571b11456216bfe7a2dd5abdd325016e372
changjiang <zhaochangjiang@modian.com>
2022-03-21 15:04:53 +0800

    12

--- lib/base/dao_get_by_ids.go
+++ lib/base/dao_get_by_ids.go
@@ -10,6 +10,10 @@ const (
 
 )
 
+var (
+	DefaultGetDataType = GetDataTypeFromDb
+)
+
 //是否刷新缓存
 const (
 	RefreshCacheNo = iota
@@ -27,16 +31,94 @@ type (
 		GetDataTypeCommon
 		Ids []int64 `json:"ids"`
 	}
-	
+
 	GetDataTypeCommon struct {
 		GetType      string `json:"get_type" form:"get_type"`
 		RefreshCache uint8  `json:"refresh_cache" form:"refresh_cache"`
 	}
+	ArgGetByStringIdsOption func(arg *ArgGetByStringIds)
+	ArgGetByNumberIdsOption func(arg *ArgGetByNumberIds)
 )
+
+// NewArgGetByStringIds
+func NewArgGetByStringIds(options ...ArgGetByStringIdsOption) (res *ArgGetByStringIds) {
+	res = &ArgGetByStringIds{}
+	for _, option := range options {
+		option(res)
+	}
+	res.GetDataTypeCommon.Default()
+	return
+}
+
+// ArgGetByStringIdsOptionGetDataTypeCommon
+func ArgGetByStringIdsOptionGetDataTypeCommon(getDataTypeCommon GetDataTypeCommon) ArgGetByStringIdsOption {
+	return func(arg *ArgGetByStringIds) {
+		arg.GetDataTypeCommon = getDataTypeCommon
+	}
+}
+
+//ArgGetByStringIdsOptionGetType
+func ArgGetByStringIdsOptionGetType(getType string) ArgGetByStringIdsOption {
+	return func(arg *ArgGetByStringIds) {
+		arg.GetType = getType
+	}
+}
+
+//ArgGetByStringIdsOptionRefreshCache
+func ArgGetByStringIdsOptionRefreshCache(refreshCache uint8) ArgGetByStringIdsOption {
+	return func(arg *ArgGetByStringIds) {
+		arg.RefreshCache = refreshCache
+	}
+}
+
+//ArgGetByStringIdsOptionIds
+func ArgGetByStringIdsOptionIds(Ids ...string) ArgGetByStringIdsOption {
+	return func(arg *ArgGetByStringIds) {
+		arg.Ids = Ids
+	}
+}
+
+//NewArgGetByNumberIds
+func NewArgGetByNumberIds(options ...ArgGetByNumberIdsOption) (res *ArgGetByNumberIds) {
+	res = &ArgGetByNumberIds{}
+	for _, option := range options {
+		option(res)
+	}
+	res.GetDataTypeCommon.Default()
+	return
+}
+
+//ArgGetByNumberIdsOptionIds
+func ArgGetByNumberIdsOptionIds(Ids ...int64) ArgGetByNumberIdsOption {
+	return func(arg *ArgGetByNumberIds) {
+		arg.Ids = Ids
+	}
+}
+
+//ArgGetByNumberIdsOptionGetDataTypeCommon
+func ArgGetByNumberIdsOptionGetDataTypeCommon(getDataTypeCommon GetDataTypeCommon) ArgGetByNumberIdsOption {
+	return func(arg *ArgGetByNumberIds) {
+		arg.GetDataTypeCommon = getDataTypeCommon
+	}
+}
+
+//ArgGetByNumberIdsOptionGetType
+func ArgGetByNumberIdsOptionGetType(getType string) ArgGetByNumberIdsOption {
+	return func(arg *ArgGetByNumberIds) {
+		arg.GetType = getType
+	}
+}
+
+//ArgGetByNumberIdsOptionRefreshCache
+func ArgGetByNumberIdsOptionRefreshCache(refreshCache uint8) ArgGetByNumberIdsOption {
+	return func(arg *ArgGetByNumberIds) {
+		arg.RefreshCache = refreshCache
+	}
+}
 
 func (r *GetDataTypeCommon) Default() (err error) {
 	if r.GetType == "" { // 默认是从缓存拿，如果拿不到，则从数据库拿
-		r.GetType = GetDataTypeFromAll
+		r.GetType = DefaultGetDataType
 	}
 
 	RefreshCacheValue := []uint8{RefreshCacheNo, RefreshCacheYes}
