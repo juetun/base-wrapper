95e756da04ec5f759cbf10f2439bf09b3fd3c231
changjiang <zhaochangjiang@modian.com>
2022-03-28 11:06:36 +0800

    12

--- lib/common/app_param/order.go
+++ lib/common/app_param/order.go
@@ -2,7 +2,9 @@ package app_param
 
 type (
 	ArgOrderFromCartItem struct {
-		SkuId string `json:"sku_id"` //sku地址
-		Num   int    `json:"num"`    //商品数量
+		SkuId  string `json:"sku_id" form:"sku_id"` //sku地址
+		Num    int    `json:"num" form:"num"`       //商品数量
+		ShopId int64  `json:"shop_id" form:"shop_id"`
+		SpuId  string `json:"spu_id" form:"spu_id"`
 	}
 )
