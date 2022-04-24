b7357cf596e788e747bd4f31f11f2dc05a4eacf6
changjiang <zhaochangjiang@modian.com>
2022-04-24 12:15:26 +0800

    添加资质类型

--- lib/common/app_param/app.go
+++ lib/common/app_param/app.go
@@ -17,14 +17,15 @@ const (
 	AppNameMallActivity     = "api-activity"
 )
 
-
 //标签类型定义
 const (
-	DataPapersGroupCategoryTag          = "user_tag"      // 用户标签
-	DataPapersGroupCategoryMallCategory = "mall_category" // 电商类目
+	DataPapersGroupCategoryTag          = "user_tag"           // 用户标签
+	DataPapersGroupCategoryMallCategory = "mall_category"      // 电商类目
+	DataPapersGroupCategoryMallBrand    = "mall_brand_quality" // 电商品牌类目
 )
 
 var MapDataPapersGroupCategory = map[string]string{
 	DataPapersGroupCategoryTag:          "用户标签",
 	DataPapersGroupCategoryMallCategory: "电商类目",
+	DataPapersGroupCategoryMallBrand:    "电商品牌",
 }
