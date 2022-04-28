460f5296199d6e83ed0fd0c69097871799e79f91
changjiang <zhaochangjiang@modian.com>
2022-04-28 10:48:27 +0800

    12

--- lib/common/app_param/user_papers.go
+++ lib/common/app_param/user_papers.go
@@ -1,5 +1,10 @@
 package app_param
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 //用户资质是否需要填写类型
 const (
 	PaperMustDateNotNeed = iota //不需要时间
@@ -11,4 +16,40 @@ var MapMustDate = map[uint8]string{
 	PaperMustDateNotNeed: "不填",
 	PaperMustDateYes:     "必填",
 	PaperMustDateNo:      "可不填",
+}
+
+const (
+	DataPapersGroupShopPropertyRadio    = "radio"    //单选
+	DataPapersGroupShopPropertyCheckbox = "checkbox" //多选
+	DataPapersGroupShopPropertySelect   = "select"   //下拉菜单
+)
+
+var MapDataPapersGroupShopProperty = map[string]string{
+	DataPapersGroupShopPropertyRadio:    "单选",
+	DataPapersGroupShopPropertyCheckbox: "多选",
+	DataPapersGroupShopPropertySelect:   "下拉菜单",
+}
+
+
+type (
+	DataPapersGroupShopProperty struct {
+		ShowType     string `json:"show_type"` //DataPapersGroupShopPropertyRadio DataPapersGroupShopPropertyCheckbox  DataPapersGroupShopPropertySelect
+		ShowTypeName string `json:"show_type_name"`
+	}
+)
+
+func (r *DataPapersGroupShopProperty) ParseShowType() (res string) {
+	var ok bool
+	if res, ok = MapDataPapersGroupShopProperty[r.ShowType]; ok {
+		return
+	}
+	res = fmt.Sprintf("未知类型(%s)", r.ShowType)
+	return
+}
+
+func (r *DataPapersGroupShopProperty) ToString() (res string, err error) {
+	var bt []byte
+	bt, err = json.Marshal(r)
+	res = string(bt)
+	return
 }
