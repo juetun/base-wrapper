1ad34650d5917a6ca811e4bc23f36ec9969def7a
zhaocj <zhaocj2009@126.com>
2023-05-22 11:22:03 +0800

    12

--- lib/common/header_info.go
+++ lib/common/header_info.go
@@ -8,20 +8,39 @@ import (
 
 type (
 	HeaderInfo struct {
-		App      string `json:"app,omitempty"`      //App类型
-		Terminal string `json:"terminal,omitempty"` //终端
-		Channel  string `json:"channel,omitempty"`  //渠道
-		Version  string `json:"version,omitempty"`  //版本
-		Debug    bool   `json:"debug,omitempty"`    //是否调试模式
-		Lng      string `json:"lng,omitempty"`      //经度
-		Lat      string `json:"lat,omitempty"`      //纬度
-		Province string `json:"pro,omitempty"`      //省
-		CityId   string `json:"city,omitempty"`     //市
-		AreaId   string `json:"area,omitempty"`     //区
+		App      string      `json:"app,omitempty"`      //App类型
+		Terminal string      `json:"terminal,omitempty"` //终端
+		Channel  string      `json:"channel,omitempty"`  //渠道
+		Version  string      `json:"version,omitempty"`  //版本
+		Debug    bool        `json:"debug,omitempty"`    //是否调试模式
+		Lng      string      `json:"lng,omitempty"`      //经度
+		Lat      string      `json:"lat,omitempty"`      //纬度
+		Province string      `json:"pro,omitempty"`      //省
+		CityId   string      `json:"city,omitempty"`     //市
+		AreaId   string      `json:"area,omitempty"`     //
+		Ext      interface{} `json:"ext,omitempty"`
 	}
 )
 
+func (r *HeaderInfo) setData(tmp *HeaderInfo) {
+	r.App = tmp.App
+	r.Terminal = tmp.Terminal
+	r.Channel = tmp.Channel
+	r.Version = tmp.Version
+	r.Debug = tmp.Debug
+	r.Lng = tmp.Lng
+	r.Lat = tmp.Lat
+	r.Province = tmp.Province
+	r.CityId = tmp.CityId
+	r.AreaId = tmp.AreaId
+	r.Ext = tmp.Ext
+	return
+}
+
 func (r *HeaderInfo) InitHeaderInfo(ctx *gin.Context) (err error) {
+	if r == nil {
+		r = &HeaderInfo{}
+	}
 	var (
 		data interface{}
 		ok   bool
@@ -33,9 +52,10 @@ func (r *HeaderInfo) InitHeaderInfo(ctx *gin.Context) (err error) {
 	switch data.(type) {
 	case HeaderInfo:
 		tmp := data.(HeaderInfo)
-		r = &tmp
+		r.setData(&tmp)
 	case *HeaderInfo:
-		r = data.(*HeaderInfo)
+		tmp := data.(*HeaderInfo)
+		r.setData(tmp)
 	default:
 		err = fmt.Errorf("系统异常,%v信息错误", app_obj.HttpHeaderInfo)
 	}
