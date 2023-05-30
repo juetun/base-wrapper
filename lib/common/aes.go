b04a942f107a89e3075c7d37899bfe47a9249653
zhaocj <zhaocj2009@126.com>
2023-05-30 09:09:12 +0800

    12

--- lib/common/aes.go
+++ lib/common/aes.go
@@ -1,7 +1,11 @@
 package common
 
 import (
+	"bytes"
+	aes2 "crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
+	"fmt"
 	"github.com/juetun/base-wrapper/lib/base"
 	"github.com/wumansgy/goEncrypt/aes"
 	"strings"
@@ -24,16 +28,49 @@ func NewAes() (res *Aes) {
 
 // EncryptionCtr 加密操作
 func (r *Aes) EncryptionCtr(text string, aesKey string) (res string, err error) {
+	var (
+		block cipher.Block
+		data  = []byte(text)
+		key   = []byte(aesKey)
+	)
+	if block, err = aes2.NewCipher(key); err != nil {
+		return
+	}
+	//判断加密快的大小
+	blockSize := block.BlockSize()
+	//填充
+	encryptBytes := pkcs7Padding(data, blockSize)
+
 	var cryptText []byte
 
-	if cryptText, err = aes.AesCtrEncrypt([]byte(text), []byte(aesKey), []byte(ChatEncryptionKey)); err != nil {
+	if cryptText, err = aes.AesCtrEncrypt(encryptBytes, key, []byte(ChatEncryptionKey)); err != nil {
 		return
 	}
 	res = base64.StdEncoding.EncodeToString(cryptText)
 	return
 }
 
+// pkcs7Padding 填充
+func pkcs7Padding(data []byte, blockSize int) []byte {
+	//判断缺少几位长度。最少1，最多 blockSize
+	padding := blockSize - len(data)%blockSize
+	//补足位数。把切片[]byte{byte(padding)}复制padding个
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(data, padText...)
+}
+
+func pkcs7UnPadding(data []byte) ([]byte, error) {
+	length := len(data)
+	if length == 0 {
+		return nil, fmt.Errorf("加密字符串错误！")
+	}
+	//获取填充的个数
+	unPadding := int(data[length-1])
+	return data[:(length - unPadding)], nil
+}
+
 func (r *Aes) DecryptCtr(text, aesKey string) (res string, err error) {
+
 	var encrypted, resBt []byte
 	if encrypted, err = base64.StdEncoding.DecodeString(text); err != nil {
 		return
@@ -42,9 +79,10 @@ func (r *Aes) DecryptCtr(text, aesKey string) (res string, err error) {
 	if resBt, err = aes.AesCtrDecrypt(encrypted, []byte(aesKey), []byte(ChatEncryptionKey)); err != nil {
 		return
 	}
+	if resBt, err = pkcs7UnPadding(resBt); err != nil {
+		return
+	}
 	res = string(resBt)
-
-	res = strings.ReplaceAll(res, "", "")
 	res = strings.ReplaceAll(res, "\b", "")
 	return
 }
--- lib/common/aes_test.go
+++ lib/common/aes_test.go
@@ -24,10 +24,10 @@ func TestAes_Encryption(t *testing.T) {
 	}{
 		{
 			args: args{
-				text:   "eyJhcHAiOiJ3ZWJzaXRlIiwiY2giOiJzaG9wIiwidiI6IjEuMCIsImRlYnVnIjp0cnVlfQ==",
+				text:   "eyJoX2FwcCI6Imp1ZXR1biIsImhfdGVybWluYWwiOiJ3ZWJzaXRlIiwiaF9jaGFubmVsIjoidXNyIiwiaF92ZXJzaW9uIjoiMS4wIiwiaF9kZWJ1ZyI6dHJ1ZX0=",
 				aesKey: "jueTungygoaesctr",
 			},
-			wantRes: "vPXu51n6dH5afcOSefKqrOnnZ2m0dH5EBNN5w/n8j8p4s430Yaewd91Yhy1Jdti5jZWBxkrweoClYl0zbTLLSQHGgpP43QuE",
+			wantRes: "vPXu4GKAc2B2V8CXasiQ59LnZzSfdEBeFIx2zNLSk8l7o9j2cZmwd/ZYhChaS9e2oY6a2Ur0X4WdSjI3RR/9DADFgozX5lnQK3ZNjKrV/cdwl1ENdBrtp/9E5mFYe9SXQAC51I0RImimZfr7W9B/C7cP6CA/07NBE/U1YcuPsZ8=",
 		},
 	}
 	for _, tt := range tests {
@@ -68,9 +68,11 @@ func TestAes_Decrypt(t *testing.T) {
 				//encrypted: "vPXu51n6dH5afcOSefKqrOnnZ2m0dH5EBNN5w/n8j8p4s430Yaewd91Yhy1Jdti5jZWBxkrweoClYl0zbTLLSQHGgpP43QuE",
 				//encrypted: "vPXu51n6dH5afcOSefKqrOnnZ2m0dH5EBNN5w/n8j8p4s430Yaewd91Yhy1Jdti5jZWBxkrweoClYl0zbTLLSQHGgpP43QuERyYL/eu0gqY=",
 				encrypted: "vPXu4GKAc2B2V8CXasiQ59LnZzSfdEBeFIx2zNLSk8l7o9j2cZmwd/ZYhChaS9e2oY6a2Ur0X4WdSjI3RR/9DADFgozX5lnQK3ZNjKrV/cdwl1ENdBrtp/9E5mFYe9SXQAC51I0RImimZfr7W9B/C7cP6CA/07NBE/U1YcuPsZ8=",
+				//encrypted: "vPXu4GKAc2B2V8CXasiQ59LnZzSfdEBeFIx2zNLSk8l7o9j2cZmwd/ZYhChaS9e2oY6a2Ur0X4WdSjI3RR/9DADFgozX5lnQFnlRgYLrvsddkiJQdnH9sf19kWRzI4+XQjrE24gSFGiLYIn/WbVnVrQY93gS8YlAKsNTMKnaiKbA/QH/kHwkcTWD77j2u0tr",
+				//encrypted: "vPXu4GKAc2B2V8CXasiQ59LnZzSfdEBeFIx2zNLSk8l7o9j2cZmwd/ZYhChaS9e2oY6a2Ur0X4WdSjI3RR/9DADFgozX5lnQFnlRgYLrvsddkiJQdnH9sf19kWRzI4+XQjrE24gSFGiLYIn/WbVnVrQY93gS8YlAKsNTMKnaiKbA/QH/kHwkcTWD77j2u0tr",
 				aesKey:    "jueTungygoaesctr",
 			},
-			wantRes: "eyJhcHAiOiJ3ZWJzaXRlIiwiY2giOiJzaG9wIiwidiI6IjEuMCIsImRlYnVnIjp0cnVlfQ==",
+			wantRes: "eyJoX2FwcCI6Imp1ZXR1biIsImhfdGVybWluYWwiOiJ3ZWJzaXRlIiwiaF9jaGFubmVsIjoidXNyIiwiaF92ZXJzaW9uIjoiMS4wIiwiaF9kZWJ1ZyI6dHJ1ZX0=",
 		},
 		//{
 		//	args: args{
