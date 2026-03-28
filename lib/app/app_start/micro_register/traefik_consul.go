a507b247415cf06ff540cae5436fa27ccffb857e
zhaocj <zhaocj2009@126.com>
2026-03-28 17:23:55 +0800

    12

--- lib/app/app_start/micro_register/traefik_consul.go
+++ lib/app/app_start/micro_register/traefik_consul.go
@@ -187,10 +187,10 @@ func (r *ConsulRegisterAndUnRegister) orgWss(middleWareCors string) (tagList []s
 	serviceRouteWss := fmt.Sprintf("wss-%v", app_obj.App.AppName)
 	tagList = []string{
 		r.orgRoutesRule(serviceRouteWss),
-		fmt.Sprintf("traefik.http.routers.%v.rule=PathPrefix(`/%v`)", serviceRouteWss, r.ConsulConfig.ServiceName), // Traefik 路由规则
-		fmt.Sprintf("traefik.http.routers.%v.service=%v", serviceRouteWss, r.ConsulConfig.ServiceName),             // Traefik 路由规则
-		fmt.Sprintf("traefik.http.routers.%s.entrypoints=%s", serviceRouteWss, "websecure"),                        // 支持websocket
-		fmt.Sprintf("traefik.http.routers.%s.middlewares=%s", serviceRouteWss, middleWareCors),                     // 设置中间件
+		fmt.Sprintf("traefik.http.routers.%v.rule=PathPrefix(`/%v`)", serviceRouteWss, r.ConsulConfig.ServiceNameNotPrefix), // Traefik 路由规则
+		fmt.Sprintf("traefik.http.routers.%v.service=%v", serviceRouteWss, r.ConsulConfig.ServiceName),                      // Traefik 路由规则
+		fmt.Sprintf("traefik.http.routers.%s.entrypoints=%s", serviceRouteWss, "websecure"),                                 // 支持websocket
+		fmt.Sprintf("traefik.http.routers.%s.middlewares=%s", serviceRouteWss, middleWareCors),                              // 设置中间件
 		//fmt.Sprintf("traefik.http.routers.%s.middlewares=%v", serviceRouteWss, fmt.Sprintf("%v,%v", middleWareCors,websocketMiddleWareName)),
 		fmt.Sprintf("traefik.http.routers.%s.tls=false", serviceRouteWss),           // 绑定入口点
 		fmt.Sprintf("traefik.tcp.routers.%v.tls.passthrough=true", serviceRouteWss), // 开启TLS 透传 Traefik 不解密 HTTPS/WSS 流量，直接把加密数据【原封不动】转发给后端服务。
