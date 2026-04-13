fa14584f4d056a089b2caa968c0456e63bb152ac
officialjuetun <zhaocj2009@126.com>
2026-04-13 21:57:38 +0800

    12

--- lib/base/cache_act/base.go
+++ lib/base/cache_act/base.go
@@ -3,6 +3,7 @@ package cache_act
 import (
 	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"github.com/juetun/base-wrapper/lib/base"
 	"runtime"
 	"time"
@@ -14,7 +15,7 @@ type (
 		Context     *base.Context
 		GetCacheKey GetCacheKey
 	}
-	GetCacheKey           func(id interface{}, expireTimeRand ...bool) (cacheKey string, duration time.Duration, err error)
+	GetCacheKey           func(id interface{}, expireTimeRand ...bool) (cacheKey string, duration time.Duration, client *redis.Client, err error)
 	CacheActionBaseOption func(cacheFreightAction *CacheActionBase)
 )
 
@@ -50,14 +51,15 @@ func (r *CacheActionBase) SetToCache(id interface{}, data interface{}, expireTim
 	var (
 		key      string
 		duration time.Duration
+		client   *redis.Client
 	)
-	if key, duration, err = r.GetCacheKey(id, expireTimeRand...); err != nil {
+	if key, duration, client, err = r.GetCacheKey(id, expireTimeRand...); err != nil {
 		return
 	}
 
-	pc, file, line, ok := runtime.Caller(1)
-	pcName := runtime.FuncForPC(pc).Name() //获取函数名
-	if err = r.Context.CacheClient.Set(r.Ctx, key, data, duration).Err(); err != nil {
+	if err = r.GetCacheClient(client).Set(r.Ctx, key, data, duration).Err(); err != nil {
+		pc, file, line, ok := runtime.Caller(1)
+		pcName := runtime.FuncForPC(pc).Name() //获取函数名
 		r.Context.Info(map[string]interface{}{
 			"id":       id,
 			"data":     data,
@@ -71,24 +73,36 @@ func (r *CacheActionBase) SetToCache(id interface{}, data interface{}, expireTim
 	return
 }
 
+func (r *CacheActionBase) GetCacheClient(clients ...*redis.Client) (client *redis.Client) {
+	var clientTmp *redis.Client
+	if len(clients) > 0 {
+		clientTmp = clients[0]
+	}
+	if clientTmp == nil {
+		clientTmp = r.Context.CacheClient
+	}
+	return
+}
+
 func (r *CacheActionBase) RemoveCacheByStringId(ids ...string) (err error) {
 	var (
 		l        = len(ids)
 		keys     = make([]string, 0, l)
 		key      string
 		duration time.Duration
+		client   *redis.Client
 	)
 	if l == 0 {
 		return
 	}
 	for _, id := range ids {
-		if key, _, err = r.GetCacheKey(id); err != nil {
+		if key, _, client, err = r.GetCacheKey(id); err != nil {
 			return
 		}
 		keys = append(keys, key)
 	}
 
-	if err = r.Context.CacheClient.Del(r.Ctx, keys...).Err(); err != nil {
+	if err = r.GetCacheClient(client).Del(r.Ctx, keys...).Err(); err != nil {
 		r.Context.Info(map[string]interface{}{
 			"ids":      ids,
 			"keys":     keys,
@@ -105,18 +119,19 @@ func (r *CacheActionBase) RemoveCacheByInterfaceId(ids ...interface{}) (err erro
 		keys     = make([]string, 0, l)
 		key      string
 		duration time.Duration
+		client   *redis.Client
 	)
 	if l == 0 {
 		return
 	}
 	for _, id := range ids {
-		if key, _, err = r.GetCacheKey(id); err != nil {
+		if key, _, client, err = r.GetCacheKey(id); err != nil {
 			return
 		}
 		keys = append(keys, key)
 	}
 
-	if err = r.Context.CacheClient.Del(r.Ctx, keys...).Err(); err != nil {
+	if err = r.GetCacheClient(client).Del(r.Ctx, keys...).Err(); err != nil {
 		r.Context.Info(map[string]interface{}{
 			"ids":      ids,
 			"keys":     keys,
@@ -133,18 +148,19 @@ func (r *CacheActionBase) RemoveCacheByNumberId(ids ...int64) (err error) {
 		keys     = make([]string, 0, l)
 		key      string
 		duration time.Duration
+		client   *redis.Client
 	)
 	if l == 0 {
 		return
 	}
 	for _, id := range ids {
-		if key, _, err = r.GetCacheKey(id); err != nil {
+		if key, _, client, err = r.GetCacheKey(id); err != nil {
 			return
 		}
 		keys = append(keys, key)
 	}
 
-	if err = r.Context.CacheClient.Del(r.Ctx, keys...).Err(); err != nil {
+	if err = r.GetCacheClient(client).Del(r.Ctx, keys...).Err(); err != nil {
 		r.Context.Info(map[string]interface{}{
 			"ids":      ids,
 			"keys":     keys,
