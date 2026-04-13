03fc4dbc97f1c1959441bb7417d09e237c5e39bd
officialjuetun <zhaocj2009@126.com>
2026-04-13 22:06:06 +0800

    12

--- lib/common/redis_pkg/cache_struct.go
+++ lib/common/redis_pkg/cache_struct.go
@@ -1,6 +1,7 @@
 package redis_pkg
 
 import (
+	"github.com/go-redis/redis/v8"
 	"github.com/juetun/base-wrapper/lib/base"
 	"time"
 )
@@ -40,13 +41,17 @@ var (
 
 type (
 	CacheProperty struct {
-		Key           string        `json:"key"`             // key
-		Expire        time.Duration `json:"expire"`          // 过期时间
-		MicroApp      string        `json:"micro_app"`       // 服务
-		CacheDataType uint8         `json:"cache_data_type"` // 缓存数据类型
-		DelKey        string        `json:"del_key"`         // 清除数据值
-		Desc          string        `json:"desc"`            // 缓存使用场景描述
+		Key      string        `json:"key"`       // key
+		Expire   time.Duration `json:"expire"`    // 过期时间
+		MicroApp string        `json:"micro_app"` // 服务
+
+		CacheDataType uint8  `json:"cache_data_type"` // 缓存数据类型
+		DelKey        string `json:"del_key"`         // 清除数据值
+		Desc          string `json:"desc"`            // 缓存使用场景描述
+
+		GetClientHandler GetCacheClientHandler `json:"-"` //获取缓存存储的节点方法,添加此方法可实现redis的存储数据打散到不同的redis存储
 	}
+	GetCacheClientHandler func(key string) (cacheClient *redis.Client, err error)
 )
 
 func (r *CacheProperty) Default() (err error) {
